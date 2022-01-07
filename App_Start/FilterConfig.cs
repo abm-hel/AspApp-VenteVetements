b04339b57f3dc6675d8e5003ce8de561ce3485c6
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2022-01-07 04:49:46 +0100

    Création du projet

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AspApp_VenteVetements
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspApp_VenteVetements.MvcApplication" Language="C#" %>
