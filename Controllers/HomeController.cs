48c9536e376fdeff031aafe454ff48595ffb8383
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2022-01-09 06:25:52 +0100

    Développemnt des fonctionnnalitées pour les utilisateurs de type 'Client'

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AspApp_VenteVetements.Models;
+using AspApp_VenteVetements.ViewModels;
+
 
 namespace AspApp_VenteVetements.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
@@ -21,11 +25,58 @@ namespace AspApp_VenteVetements.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        
+        public ActionResult AfficherCommandes()
         {
-            ViewBag.Message = "Your contact page.";
+            if (User.Identity.IsAuthenticated == true && User.IsInRole("2"))
+            {
+                using (ContexteBD baseDonnees = new ContexteBD())
+                {
+                    var identifiantUtilisateur = (from utilisateur in baseDonnees.Utilisateurs
+                                                  where utilisateur.adresseMail == User.Identity.Name
+                                                  select utilisateur.id).SingleOrDefault();
 
-            return View();
+                    string nomUtilisateur = (from utilisateur in baseDonnees.Utilisateurs
+                                                  where utilisateur.adresseMail == User.Identity.Name
+                                                  select utilisateur.nom).SingleOrDefault();
+
+                    ViewBag.messageBienvenue = "Bonjour monsieur" + " " + nomUtilisateur;
+
+                    var mesCommandes = (from lesCommandes in baseDonnees.Commandes
+                                     where lesCommandes.utilisateurId == identifiantUtilisateur
+                                     select lesCommandes).ToList();
+
+                    var mesVetememenCommandes = (from lesVetements in baseDonnees.Vetements
+                                                 join lesVetementsCommandes in baseDonnees.VetementCommandes on lesVetements.id equals lesVetementsCommandes.vetementId
+                                                 join lesCommandes in baseDonnees.Commandes on lesVetementsCommandes.commandeId equals lesCommandes.id
+                                                 where lesCommandes.utilisateurId == identifiantUtilisateur
+                                                 select new {lesVetementsCommandes.quantite, lesCommandes.id, lesVetements}).ToList();
+
+                    List<VetementsAchetes> c = new List<VetementsAchetes>();
+                    
+                    foreach (var vc in mesVetememenCommandes)
+                    {
+                        VetementsAchetes l = new VetementsAchetes();
+                        l.Commande = vc.id;
+                        l.InfosVetements = vc.lesVetements;
+                        l.Quantite = vc.quantite;
+                        c.Add(l);
+                    }
+
+                    var monViewModelCommandes = new CommandeViewModel
+                    {   Commandes = mesCommandes,
+                        Vetements = c
+                    };
+                    
+             
+
+                    return View(monViewModelCommandes);
+                }
+            }
+            else
+            {
+                return RedirectToAction("Connexion", "Authentification");
+            }
         }
     }
 }
---
+++ ViewModels/CommandeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using AspApp_VenteVetements.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspApp_VenteVetements.ViewModels
+{
+    public class CommandeViewModel
+    {
+        public List<Commande> Commandes { get; set; }
+        public List<VetementsAchetes> Vetements { get; set; }
+    }
+}
+
+public class VetementsAchetes
+{
+    public int Quantite { get; set; }
+    public int Commande {get; set;}
+    public Vetement InfosVetements { get; set; }
+
+}
